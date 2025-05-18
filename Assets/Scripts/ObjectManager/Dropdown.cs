8f97e3cf88d31a111a58029d6f9d540fee61ba3e
ja3527123 <ja3527123@gmail.com>
2025-05-18 14:55:53 +0800

    改錯字

--- Assets/Scripts/ObjectManager/Dropdown.cs
+++ Assets/Scripts/ObjectManager/Dropdown.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Dropdown : MonoBehaviour
 {
-    publicb float fallSpeed = 3;
+    public float fallSpeed = 3;
     public float destroyTime = 5;
     Rigidbody2D rb;
 
