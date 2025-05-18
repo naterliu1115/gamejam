88e388619bec1a7aefd85fd8a68147c3bdbe35ba
mulatin <107930748+mulatin@users.noreply.github.com>
2025-05-18 15:21:44 +0800

    修改場景

--- Assets/Scripts/ObjectManager/Dropdown.cs
+++ Assets/Scripts/ObjectManager/Dropdown.cs
@@ -2,19 +2,19 @@ using UnityEngine;
 
 public class Dropdown : MonoBehaviour
 {
-    publicb float fallSpeed = 3;
+    public float fallSpeed = 3;
     public float destroyTime = 5;
-    Rigidbody2D rb;
+    
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        
         Destroy(gameObject,destroyTime);
     }
 
     void Update()
     {
-        transform.position = Vector2.down;
+        transform.Translate(Vector3.down * fallSpeed * Time.deltaTime);
     }
 
 }
