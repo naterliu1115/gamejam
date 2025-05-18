7dc7719e810da54cb584ce949dcddb2a4c12a3df
mulatin <107930748+mulatin@users.noreply.github.com>
2025-05-18 14:48:47 +0800

    更新 掉落速度

--- Assets/Scripts/ObjectManager/Dropdown.cs
+++ Assets/Scripts/ObjectManager/Dropdown.cs
@@ -2,13 +2,19 @@ using UnityEngine;
 
 public class Dropdown : MonoBehaviour
 {
-    
+    publicb float fallSpeed = 3;
     public float destroyTime = 5;
-    
+    Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
         Destroy(gameObject,destroyTime);
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.down;
     }
 
 }
